e690684a23622735129b3be0863b21718426f7f5
Alexandre <alexandrecadene@outlook.com>
2014-01-30 12:59:40 +0100

    Ajout du fichier principal
    Ajouter du fichier javascript

---
+++ jquery.menuHighlight.js
@@ -0,0 +1,44 @@
+(function($)
+{
+	$.fn.menuHighlight=function(options)
+	{
+
+		var defauts =
+		{
+			"onglet" : ".onglet"
+		}
+
+		var parametres=$.extend(defauts, options);
+
+		this.each(function()
+		{
+			var element = $(this);
+			var urlTmp = document.location.href.split('/');
+			var urlSite = urlTmp[urlTmp.length - 1];
+			var urlSousNav = $(parametres.onglet + ' li:first-child a').attr('href');
+
+			element.find('a').each(function() {
+
+				var url = $(this).attr('href');
+
+				if (urlSite == url || urlSousNav == url) {
+					$(this).parents('li').children().addClass('active');
+				}
+
+			});
+
+			$(parametres.onglet).find('a').each(function() {
+
+				var url = $(this).attr('href');
+
+				if (urlSite == url) {
+					$(this).addClass('active');
+				}
+
+			});
+
+		});
+
+		return this;
+	}
+})(jQuery);
